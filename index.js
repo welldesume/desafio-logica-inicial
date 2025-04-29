35cf0c208ac2a9cb78be8931a48370f0068e431f
welldesume <wwelligtontrabalho@gmail.com>
2025-04-28 22:11:08 -0300

    modificaçoes apois a clonagem

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 console.log("Ola Mundão!");
+console.log("no git hub")
+console.log("no repositorio")
+console.log("na opcao code HTTPS")
+console.log("copiei")
+console.log("na meu pc criei uma nova pasta e abri pelo vs code")
+console.log("digitei no terminal do vs code  git clone e endereco copiado")
+console.log("depois node e o endeco da arquino tipo")
+console.log("node .\desafio-logica\inicial")
+console.log("e pronto")
+console.log("paraa fazer modificaoes tem de fazer todo o processo")
+console.log("refaz os comandos add commit push")
+
+
